13bd290235f62f8d353957fb4f2a4420bdfe991d
Ryan <mozielin@gmail.com>
2021-09-28 22:04:07 +0800

    ADD Default

--- src/publishable/config/sso.php
+++ src/publishable/config/sso.php
@@ -2,7 +2,7 @@
 
 return [
     // SSO credentials
-    'sso_enable'    => env('SSO_ENABLE'),
+    'sso_enable'    => env('SSO_ENABLE',false),
     'client_secret' => env("SSO_CLIENT_SECRET"),
     'callback'      => env("SSO_CLIENT_CALLBACK"),
     'sso_host'      => env("SSO_HOST")
